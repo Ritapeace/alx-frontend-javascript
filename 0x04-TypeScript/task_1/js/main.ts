1802bc16649e31da3d5ce24d4b4a240b88b8f3ad
Ritapeace <ritapeace1997@gmail.com>
2023-01-04 19:10:15 -0600

    CLass teacher

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,53 @@
+//Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any
+}
+
+//Directors interface
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working'
+    }
+
+    displayName(): string {
+        return this.firstName
+    }
+}
+
+const studentClass: StudentClass = new StudentClass('Sarah', 'Mapenzi');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
